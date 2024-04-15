fc8e732fa93ef59f4b673496f362ce8d25116f7e
mdavidmontero <mdavidmontero6@gmail.com>
2024-04-15 11:54:10 -0500

    deployt

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize-typescript";
 import dotenv from "dotenv";
 dotenv.config();
 const db = new Sequelize(process.env.DATABASE_URL!, {
-  models: [__dirname + "/../models/**/*.ts"],
+  models: [__dirname + "/../models/**/*"],
   // logging: false, //para test
 });
 
